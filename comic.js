4dbcfeeed27f98f76eec35c2bb0fbce3136aa62c
Juha Tauriainen <juha@bin.fi>
2018-05-08 14:33:09 +0300

    Update comic.js

--- comic.js
+++ comic.js
@@ -1,6 +1,6 @@
-document.body.innerHTML += 
-	'<style>* { font-family: "Comic Sans MS" !important;} ' + 
-	'p:nth-child(odd) {color: pink !important;} ' +
-	'p:nth-child(even) {color: red !important;} ' +
-	'li:nth-child(even) {color: orange !important;}' +
-	'</style>';
+document.body.innerHTML +=
+  '<style>* { font-family: "Comic Sans MS" !important; background-image: -webkit-linear-gradient(right, #777dff 0px, #f154ff 200px, #f0357c 400px),-webkit-linear-gradient(left, #777dff 0px, #497ce2 200px, #37bdde 400px);background-image: linear-gradient(to left, #777dff 0px, #f154ff 200px, #f0357c 400px),linear-gradient(to right, #777dff 0px, #497ce2 200px, #37bdde 400px);background-size: 51% 100%;background-position: top left, top right;background-repeat: no-repeat;-webkit-background-clip: text; -webkit-text-fill-color: transparent;} ' +
+  'p:nth-child(odd) {color: pink !important;} ' +
+  'p:nth-child(even) {color: red !important;} ' +
+  'li:nth-child(even) {color: orange !important;}' +
+  '</style>';
